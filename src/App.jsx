aac74707776aeba187167d74cc3834781275f803
Kenneth <kenneth1.kodehode@gmail.com>
2022-12-18 15:07:54 +0100

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { useTimer } from "react-timer-and-stopwatch";
 
 function App() {
 
   return (
+    <>
     <div className="App">
+      
+    <Countdown />
       {/* Carousel */}
 <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
@@ -35,9 +39,64 @@ function App() {
     </div>
   </div>
 </div>
+<div className="stats shadow">
+  
+  <div className="stat">
+    <div className="stat-figure text-primary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+    </div>
+    <div className="stat-title">Total Likes</div>
+    <div className="stat-value text-primary">25.6K</div>
+    <div className="stat-desc">21% more than last month</div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+    </div>
+    <div className="stat-title">Page Views</div>
+    <div className="stat-value text-secondary">2.6M</div>
+    <div className="stat-desc">21% more than last month</div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      <div className="avatar online">
+        <div className="w-16 rounded-full">
+          <img src="https://placeimg.com/128/128/people" />
+        </div>
+      </div>
+    </div>
+    <div className="stat-value">86%</div>
+    <div className="stat-title">Tasks done</div>
+    <div className="stat-desc text-secondary">31 tasks remaining</div>
+  </div>
+  
+</div>
+  
 
     </div>
+    
+    </>
   )
 }
 
 export default App
+
+
+
+const Countdown = () => {
+  const timer = useTimer({
+      create: {
+          timerWithDuration: {
+              time: { // Set a duration of 1 minute and 30 seconds
+                  minutes: 5,
+                  seconds: 0
+              }
+          }
+      }
+  });
+  return (
+      <span className="countdown font-mono text-6xl">Time Left: {timer.timerText}</span>
+  );
+}
